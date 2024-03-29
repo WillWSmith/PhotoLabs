ccd8c39f4d02959f696b65db6e386cd0b913cc36
WillWSmith <will.w.smith94@gmail.com>
2024-03-29 10:59:45 -0400

    Apply css styling to PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 
-const PhotoListItem = ({photo}) => {
+const PhotoListItem = ({ photo }) => {
   return (
-    <>
-    <img src={photo.imageSource} alt="Photo"/>
-    <div>
-    <img src={photo.profile}/>
-    <h3>{photo.username}</h3>
-    <p>{photo.location.city}, {photo.location.country}</p>
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={photo.imageSource} alt="Photo"/>
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={photo.profile} alt="User Profile"/>
+        <div className="photo-list__user-info">
+          <h3>{photo.username}</h3>
+          <p className="photo-list__user-location">{photo.location.city}, {photo.location.country}</p>
+        </div>
+      </div>
     </div>
-    </>
   )
 };
 
 export default PhotoListItem;
+
