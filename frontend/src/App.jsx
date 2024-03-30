d9bb5ac656ced78cd9c0e7693fda9783a2382f86
WillWSmith <will.w.smith94@gmail.com>
2024-03-29 22:04:12 -0400

    Start of PhotoList component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,16 @@
 import React from 'react';
-
+import PhotoList from './components/PhotoList';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-// Note: Rendering a single component to build components in isolation
 const App = () => {
-  const photos = [
-    {
-      id: "1",
-      location: {
-        city: "Montreal",
-        country: "Canada",
-      },
-      imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-      username: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-    {
-      id: "2",
-      location: {
-        city: "Montreal",
-        country: "Canada",
-      },
-      imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-      username: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-    {
-      id: "3",
-      location: {
-        city: "Montreal",
-        country: "Canada",
-      },
-      imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-      username: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    }
-  ]
+
   return (
     <div className="App">
-      {photos.map(photo => (
+      {/* {photos.map(photo => (
         <PhotoListItem key={photo.id} photo={photo} />
-      ))}
+      ))} */}
+      <PhotoList/>
     </div>
   );
 };
