7048fc0fa32e6f398d567827a7cd2347a26b5d4a
WillWSmith <will.w.smith94@gmail.com>
2024-03-29 22:12:53 -0400

    Refactor PhotoList item

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,11 +6,11 @@ const PhotoListItem = ({ photo }) => {
   return (
     <div className="photo-list__item">
       <PhotoFavButton/>
-      <img className="photo-list__image" src={photo.imageSource} alt="Photo"/>
+      <img className="photo-list__image" src={photo.urls.regular} alt="Photo"/>
       <div className="photo-list__user-details">
-        <img className="photo-list__user-profile" src={photo.profile} alt="User Profile"/>
+        <img className="photo-list__user-profile" src={photo.user.profile} alt="User Profile"/>
         <div className="photo-list__user-info">
-          <div>{photo.username}</div>
+          <div>{photo.user.name}</div>
           <div className="photo-list__user-location">{photo.location.city}, {photo.location.country}</div>
         </div>
       </div>
