616c5d0abe6c979e177139e129a1b4cc087503c1
WillWSmith <will.w.smith94@gmail.com>
2024-04-02 11:20:13 -0400

    Refactor PhotoDetailsModal format

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -8,35 +8,36 @@ const PhotoDetailsModal = ({ setDisplayModal, photoDetails, similarPhotos }) => 
   const similarPhotosArray = Array.isArray(similarPhotos) ? similarPhotos : Object.values(similarPhotos || {});
 
   return (
-    <div className="photo-details-modal">
-      <div className="photo-details-modal__top-bar">
-        <button className="photo-details-modal__close-button" onClick={closeModal}>
-          <img src={closeSymbol} alt="Close" />
-        </button>
-        <div className="photo-details-modal__photographer-details">
-          <img 
-            src={photoDetails.user.profile} 
-            alt={`${photoDetails.user.name}'s profile`} 
-            className="photo-details-modal__photographer-profile"
-          />
-          <div>
-            <div className="photo-details-modal__photographer-info">{photoDetails.user.name}</div>
-            {photoDetails.location.city && (
-              <div className="photo-details-modal__photographer-location">{photoDetails.location.city}, {photoDetails.location.country}</div>
-            )}
-          </div>
-        </div>
-      </div>
-      <img 
-        className="photo-details-modal__image" 
-        src={photoDetails.urls.full} 
-        alt={photoDetails.description || 'Photo description unavailable'} 
-      />
-      <h3 className="photo-details-modal__header">Similar Photos</h3>
-      <div className="photo-details-modal__images">
-        <PhotoList photos={similarPhotosArray} />
-      </div>
+<div className="photo-details-modal">
+  <div className="photo-details-modal__top-bar">
+    <button className="photo-details-modal__close-button" onClick={closeModal}>
+      <img src={closeSymbol} alt="Close" />
+    </button>
+  </div>
+  <img 
+    className="photo-details-modal__image" 
+    src={photoDetails.urls.full} 
+    alt={photoDetails.description || 'Photo description unavailable'} 
+  />
+  <div className="photo-details-modal__photographer-details">
+    <img 
+      src={photoDetails.user.profile} 
+      alt={`${photoDetails.user.name}'s profile`} 
+      className="photo-details-modal__photographer-profile"
+    />
+    <div>
+      <div className="photo-details-modal__photographer-info">{photoDetails.user.name}</div>
+      {photoDetails.location.city && (
+        <div className="photo-details-modal__photographer-location">{photoDetails.location.city}, {photoDetails.location.country}</div>
+      )}
     </div>
+  </div>
+
+  <h3 className="photo-details-modal__header">Similar Photos</h3>
+  <div className="photo-details-modal__images">
+    <PhotoList photos={similarPhotosArray} />
+  </div>
+</div>
   );
 };
 
